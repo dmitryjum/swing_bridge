bf2a547ae3a633d1011b70edc5e7e585f09e4323
Dmitry Jum <xfiler@ymail.com>
2026-01-22 03:14:17 +0400

    feat: enable html layout and admin base controller

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,6 +1,5 @@
 class Admin::BaseController < ActionController::Base
   layout "admin"
-
   unless ENV["ADMIN_AUTH_DISABLED"] == "true"
     http_basic_authenticate_with(
       name: Rails.application.credentials.dig(:admin, :http_basic_auth_user),
