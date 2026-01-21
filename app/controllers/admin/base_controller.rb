0f027352b8db30a86a4564c2c1488e36de52171a
Dmitry Jum <xfiler@ymail.com>
2026-01-22 03:39:23 +0400

    fix: make admin layout standalone and auth toggle

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,8 +1,10 @@
 class Admin::BaseController < ActionController::Base
   layout "admin"
 
-  http_basic_authenticate_with(
-    name: Rails.application.credentials.dig(:admin, :http_basic_auth_user),
-    password: Rails.application.credentials.dig(:admin, :http_basic_auth_password)
-  )
+  unless ENV["ADMIN_AUTH_DISABLED"] == "true"
+    http_basic_authenticate_with(
+      name: Rails.application.credentials.dig(:admin, :http_basic_auth_user),
+      password: Rails.application.credentials.dig(:admin, :http_basic_auth_password)
+    )
+  end
 end
